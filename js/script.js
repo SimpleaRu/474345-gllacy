fe5bfa24e842070a90f4964200babc2016938d4d
SimpleaRU <info@simplea.ru>
2017-12-09 11:21:29 +0300

    final_check_0.3

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@ var modalForm = document.querySelector(".feed-back");
 var feedBackButton = document.querySelector("#feed-back-show");
 var feedBackClose = document.querySelector(".feed-close-button");
 
-feedBackButton.addEventListener('click', (e) => {
+feedBackButton.addEventListener('click', function(e) {
     e.preventDefault();
     modalForm.classList.add("feed-back-show");
 });
 
-feedBackClose.addEventListener('click', (e) => {
+feedBackClose.addEventListener('click', function(e) {
     e.preventDefault();
     modalForm.classList.remove("feed-back-show");
 });
